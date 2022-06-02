161c15ac8acd07765dba03748450fd27b80c3d52
Zunuoh Ponu <ns13030133@gmail.com>
2022-06-02 14:12:08 +0000

    contact us screen implemented

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Homescreen from "./components/homescreen/Homescreen";
+// import Homescreen from "./components/homescreen/Homescreen";
+import ContactusScreen from "./components/contact/ContactusScreen";
 
 function App() {
-  return <Homescreen />;
+  return <ContactusScreen/>;
 }
 
 export default App;
---
+++ src/components/contact/ContactusScreen.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import Header from '../Header'
+import {Row, Col} from 'react-bootstrap'
+import ContactusImage from "../../assets/contactus.png"
+import ContactusForm from './ContactusForm'
+
+const ContactusScreen = () => {
+    return (
+        <div>
+           <Header/>
+           <div>
+                <Row>
+                    <Col sm={7} style={{backgroundColor:""}}>
+                        <div className="contactusDiv">
+                            <div className="contactusIconDiv">
+                            <img src={ContactusImage} className="contactusIcon"/>
+                            </div>
+                            <div className="contactusTextContainer">
+                                <div className="contactusHeader">
+                                Contact us
+                                </div>
+                                <div className="contactusText">
+                                    Need to get in touch with us? Either fill out the form with your enquiry of find the department email you'd like to contact below.
+                                </div>
+                                
+                            </div>
+                        </div>
+                    </Col>
+                    <Col sm={5} style={{backgroundColor:"#faf8f0",height:"92vh"}}>
+                        <ContactusForm/>
+                    </Col>
+                </Row>
+          </div>
+        </div>
+    )
+}
+
+export default ContactusScreen;
